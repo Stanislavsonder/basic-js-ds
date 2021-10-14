532ab2f71762450aedfb4a70568a827d9394e0a4
Stanislavsonder <Stanislavsonder@gmail.com>
2021-10-14 17:59:44 +0300

    Update binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,119 @@
-const { NotImplementedError } = require('../extensions/index.js');
+import { NotImplementedError } from '../extensions/index.js';
 
-// const { Node } = require('../extensions/list-tree.js');
+// import { Node } from '../extensions/list-tree.js';
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-module.exports = class BinarySearchTree {
+
+function Node(value) {
+  this.data = value
+  this.left = null
+  this.right = null
+}
+
+export default class BinarySearchTree {
+
+  #root = null
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#root
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+    if (!this.#root) {
+      this.#root = new Node(value)
+      return
+    }
+    let node = this.#root
+    let parent
+    while (node) {
+      parent = node
+      node = value < node.data? node.left:node.right
+      if (!node) {
+        value < parent.data?
+            parent.left = new Node(value):
+            parent.right = new Node(value)
+        break
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(value) {
+    let node = this.#root
+    while (node) {
+      if (node.data === value) return true
+      node = value < node.data? node.left : node.right
+    }
+    return false
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(value) {
+    let node = this.#root
+    while (node) {
+      if (node.data === value) return node
+      node = value < node.data? node.left : node.right
+    }
+    return null
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(value) {
+    if (!this.#root) return
+
+
+    let curr = this.#root
+    let parent = null
+
+    while (curr && curr.data != value){
+      parent = curr
+      if (curr.data > value) curr = curr.left
+      else curr = curr.right
+    }
+
+    if (!curr) return
+
+    if (curr.left == null) {
+      console.log('Вместо curr подвешивается его правое поддерево')
+      if (parent && parent.left == curr) parent.left = curr.right
+      if (parent && parent.right == curr) parent.right = curr.right
+      curr = null
+      return
+    }
+
+    if (curr.right == null) {
+      console.log('Вместо curr подвешивается его левое поддерево')
+      if (parent && parent.left == curr) parent.left = curr.left;
+      if (parent && parent.right == curr) parent.right = curr.left;
+      curr = null
+      return
+    }
+
+
+    console.log('У элемента есть два потомка, тогда на место элемента поставим наименьший элемент из его правого поддерева')
+    let replace = curr.right
+    while (replace.left) replace = replace.left
+    let replaceValue = replace.data
+    this.remove(replaceValue)
+    curr.data = replaceValue
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.#root) return null
+    let node = this.#root
+    while (node.left){
+      node = node.left
+    }
+    return node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.#root) return null
+    let node = this.#root
+    while (node.right){
+      node = node.right
+    }
+    return node.data
   }
 
 }
